b5c9df5cb4c8b41c7508440c83060bea18b51a82
Erik <erik@145.137.EARLY-REGISTRATION.of.SURFnet.invalid>
2020-02-17 11:45:59 +0100

    fix typo

--- week1/lesvoorbeeld/games.php
+++ week1/lesvoorbeeld/games.php
@@ -1,8 +1,12 @@
 <?php
 header('Content-Type:application/json');
 $jsonData = file_get_contents("games.json");
-$json = json_decode($jsonData, true);
-//print_r($json);
-echo json_encode($json);
-//echo $jsonData;
+
+// Om de data in PHP te kunnen bewerken gebruik je json_decode
+// Als je klaar bent met bewerken gebruik je json_encode voordat je het verstuurt
+// $json = json_decode($jsonData, true);
+// print_r($json);
+// echo json_encode($json);
+
+echo $jsonData;
 exit;
